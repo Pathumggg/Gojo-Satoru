a617943b1038b1da81169d33708f06a55e09b904
Pathumggg <109660579+Pathumggg@users.noreply.github.com>
2022-07-21 10:28:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['Your Number94760390292']
+global.premium = ['Your Number94760390292']
+global.ownernomer = 'Your Number94760390292'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.botname = 'pathum'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
